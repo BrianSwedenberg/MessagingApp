4ccaf7f9b590541ed3bfbe1cdc4398d165324f96
BrianSwedenberg <bswedenberg@gmail.com>
2025-04-24 13:49:32 +0000

    changed App to router

--- messaging-app/App.tsx
+++ messaging-app/App.tsx
@@ -1,15 +1,15 @@
-// filepath: /workspaces/MessagingApp/messaging-app/App.tsx
-import { View, Text } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24 }}>Hello from App this is Brian's Dumb attempt</Text>
-    </View>
-  );
-}
-// import { Slot } from 'expo-router';
+// // filepath: /workspaces/MessagingApp/messaging-app/App.tsx
+// import { View, Text } from 'react-native';
 
 // export default function App() {
-//   return <Slot />;
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text style={{ fontSize: 24 }}>Hello from App this is Brian's Dumb attempt</Text>
+//     </View>
+//   );
 // }
+import { Slot } from 'expo-router';
+
+export default function App() {
+  return <Slot />;
+}
