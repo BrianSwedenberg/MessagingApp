277b8dd8c80df6995d523d31dfa5d23ea401c86d
BrianSwedenberg <bswedenberg@gmail.com>
2025-04-24 13:33:10 +0000

    updated and deleted env files

--- messaging-app/app/index.tsx
+++ messaging-app/app/index.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 
-export default function HomeScreen() {
+export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24 }}>Hello from Home</Text>
